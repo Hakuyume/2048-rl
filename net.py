75eb9e4859255be66a707e20b25fe9405137b851
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-08-23 10:14:25 +0900

    going deeper

--- net.py
+++ net.py
@@ -11,7 +11,8 @@ class CNN(chainer.Chain):
         super().__init__()
         with self.init_scope():
             self.conv1 = L.Convolution2D(n_channel, 3, pad=1)
-            self.conv2 = L.Convolution2D(4, 3, pad=1)
+            self.conv2 = L.Convolution2D(n_channel, 3, pad=1)
+            self.conv3 = L.Convolution2D(4, 3, pad=1)
 
     def __call__(self, x):
         xp = self.xp
@@ -19,12 +20,12 @@ class CNN(chainer.Chain):
         h = (x[:, None] == (xp.arange(1 + 16))[:, None, None]) \
             .astype(np.float32)
         h = F.relu(self.conv1(h))
-        h = self.conv2(h)
-        h = _global_max_pooling_2d(h)
+        h = F.relu(self.conv2(h))
+        h = _global_average_pooling_2d(self.conv3(h))
         return F.reshape(h, (-1, 4))
 
 
-def _global_max_pooling_2d(x):
-    h = F.max(x, axis=2, keepdims=True)
-    h = F.max(h, axis=3, keepdims=True)
+def _global_average_pooling_2d(x):
+    h = F.mean(x, axis=2, keepdims=True)
+    h = F.mean(h, axis=3, keepdims=True)
     return h
